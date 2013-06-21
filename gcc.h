f1d0fe5724eb0c072fabfca428e2e48f758698df
Wang Luming <wlm199558@126.com>
2013-06-21 20:59:56 +0800

    partly accomplished gcc support

---
+++ gcc.h
@@ -0,0 +1,27 @@
+#ifndef GCC_H
+#define GCC_H
+
+#include <QFileInfo>
+#include <QDebug>
+
+#include "compilerbase.h"
+
+class gcc : public compilerBase
+{
+    Q_OBJECT
+public:
+    explicit gcc(QObject *parent = 0);
+    void startCompile(const QString &filePath);
+    QString version();
+    QString path(){return gccPath;}
+    bool checkCompilerPath(const QString& ){return true;}
+    QString compilerName(){return "g++";}
+
+public slots:
+    void onOutputReady();
+
+private:
+    static QString gccPath;
+};
+
+#endif // GCC_H
