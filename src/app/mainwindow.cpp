007cadad7596315be60b517d85df767fc7310343
Wang Luming <wlm199558@126.com>
2013-07-12 17:59:44 +0800

    fix a small Drag & Drop bug on Linux platform

--- src/app/mainwindow.cpp
+++ src/app/mainwindow.cpp
@@ -750,7 +750,11 @@ void MainWindow::dropEvent(QDropEvent *event)
     while(i--)
     {
         tmpPath=fileList.at(i).path();
+
+#ifdef Q_OS_WIN32
         tmpPath=tmpPath.remove(0,1);
+#endif
+
         tabManager->openAndJumpTo(tmpPath);
     }
 }
