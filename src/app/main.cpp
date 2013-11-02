eee84a761e020d5134543d50209b35e7155c92ac
Saki <krenedev@126.com>
2013-11-02 23:00:49 +0800

    Major: 0.2.0.0 Updates: 1. Version. 2. Assistant documents. 3. Windows Details resource file.

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -40,7 +40,7 @@
 static inline void setApplicationInfo()
 {
     QApplication::setApplicationName(QString("Cuties"));
-    QApplication::setApplicationVersion(QString("0.1.0.1"));
+    QApplication::setApplicationVersion(QString("0.2.0.0"));
     QApplication::setOrganizationName("Kreogist Dev Team");
     QApplication::setOrganizationDomain("https://kreogist.github.io/Cuties");
 
