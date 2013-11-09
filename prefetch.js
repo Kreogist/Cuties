a488b8569bc9c7552b3822264bfd29eb6d7b6e64
frantic1048 <franticblack@163.com>
2013-11-09 17:49:02 +0800

    remove useless stuff

--- prefetch.js
+++ prefetch.js
@@ -1,11 +1,11 @@
- new Image().src = 'http://kreogist.github.io/Cuties/btn_home_hover.png';
- new Image().src = 'http://kreogist.github.io/Cuties/btn_features_hover.png';
- new Image().src = 'http://kreogist.github.io/Cuties/btn_download_hover.png';
- new Image().src = 'http://kreogist.github.io/Cuties/btn_support_hover.png';
- new Image().src = 'http://kreogist.github.io/Cuties/btn_faq_hover.png';
- 
- new Image().src = 'http://kreogist.github.io/Cuties/btn_home_press.png';
- new Image().src = 'http://kreogist.github.io/Cuties/btn_features_press.png';
- new Image().src = 'http://kreogist.github.io/Cuties/btn_download_press.png';
- new Image().src = 'http://kreogist.github.io/Cuties/btn_support_press.png';
- new Image().src = 'http://kreogist.github.io/Cuties/btn_faq_press.png';
+/*prefetch img for left buttons*/
+new Image().src = 'http://kreogist.github.io/Cuties/btn_home_hover.png';
+new Image().src = 'http://kreogist.github.io/Cuties/btn_features_hover.png';
+new Image().src = 'http://kreogist.github.io/Cuties/btn_download_hover.png';
+new Image().src = 'http://kreogist.github.io/Cuties/btn_support_hover.png';
+new Image().src = 'http://kreogist.github.io/Cuties/btn_faq_hover.png';
+new Image().src = 'http://kreogist.github.io/Cuties/btn_home_press.png';
+new Image().src = 'http://kreogist.github.io/Cuties/btn_features_press.png';
+new Image().src = 'http://kreogist.github.io/Cuties/btn_download_press.png';
+new Image().src = 'http://kreogist.github.io/Cuties/btn_support_press.png';
+new Image().src = 'http://kreogist.github.io/Cuties/btn_faq_press.png';
