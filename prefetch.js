fcfd0379c55ef26caadab4a6c0446e5935ab1691
Saki <tomguts@126.com>
2013-12-13 10:07:13 +0800

    New Preload test

--- prefetch.js
+++ prefetch.js
@@ -1,4 +1,8 @@
 /*prefetch img for left buttons*/
+window.onload = function(){ 
+	new Image().src= 'http://kreogist.github.io/Cuties/fonts/sao.otf';
+	new Image().src= 'http://kreogist.github.io/Cuties/fonts/SourceSansPro-ExtraLight.otf';
+	new Image().src= 'http://kreogist.github.io/Cuties/fonts/SourceSansPro-Regular.otf';
 new Image().src = 'http://kreogist.github.io/Cuties/btn_home_hover.png';
 new Image().src = 'http://kreogist.github.io/Cuties/btn_features_hover.png';
 new Image().src = 'http://kreogist.github.io/Cuties/btn_download_hover.png';
@@ -9,3 +13,9 @@ new Image().src = 'http://kreogist.github.io/Cuties/btn_features_press.png';
 new Image().src = 'http://kreogist.github.io/Cuties/btn_download_press.png';
 new Image().src = 'http://kreogist.github.io/Cuties/btn_support_press.png';
 new Image().src = 'http://kreogist.github.io/Cuties/btn_faq_press.png';
+new Image().src = 'http://kreogist.github.io/Cuties/btn_home_normal.png';
+new Image().src = 'http://kreogist.github.io/Cuties/btn_features_normal.png';
+new Image().src = 'http://kreogist.github.io/Cuties/btn_download_normal.png';
+new Image().src = 'http://kreogist.github.io/Cuties/btn_support_normal.png';
+new Image().src = 'http://kreogist.github.io/Cuties/btn_faq_normal.png';
+}
