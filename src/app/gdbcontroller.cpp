30b8840758ffa73caaf8a1b105156621cc09d9cc
Saki <krenedev@126.com>
2013-12-15 20:00:51 +0800

    Minor: 0.2.5.16
    Bug Fixed:
    1. Fixed strange bug on Windows. Qt cann't konw #ifndef Q_OS_WIN!!!!!

--- src/app/gdbcontroller.cpp
+++ src/app/gdbcontroller.cpp
@@ -20,23 +20,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#ifndef Q_OS_WIN
-#include <QSocketNotifier>
-#include <QTemporaryFile>
-#include <QVarLengthArray>
-
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#ifdef Q_OS_SOLARIS
-# include <sys/filio.h> // FIONREAD
-#endif
-#include <errno.h>
-#include <fcntl.h>
-#include <string.h>
-#include <unistd.h>
-#endif
-
 #include "gdbcontroller.h"
 
 #ifdef Q_OS_UNIX
@@ -45,6 +28,20 @@ QString GdbController::gdbPath="/usr/bin/gdb";
 
 #ifdef Q_OS_WIN32
 QString GdbController::gdbPath="C:/MinGW/bin/gdb.exe";
+#else
+#include <QSocketNotifier>
+#include <QTemporaryFile>
+#include <QVarLengthArray>
+#include <sys/ioctl.h>
+#ifdef Q_OS_SOLARIS
+# include <sys/filio.h> // FIONREAD
+#endif
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
 #endif
 
 bool GdbController::checkResult=false;
