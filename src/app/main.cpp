bd3721c229a706179d0b145875793de765cc4321
Saki <krenedev@126.com>
2014-01-24 15:28:01 +0800

    Major: 0.3.0.4 Updates: 1. Merge conflict.
    Bug Fixed:
    1. Fixed main window crashed bug.

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -50,7 +50,7 @@ static inline void loadApplicationInfo()
 {
     //Set application details.
     QApplication::setApplicationName(QString("Cuties"));
-    QApplication::setApplicationVersion(QString("0.3.0.3"));
+    QApplication::setApplicationVersion(QString("0.3.0.4"));
     QApplication::setOrganizationName("Kreogist Dev Team");
     QApplication::setOrganizationDomain("https://kreogist.github.io/Cuties");
 
