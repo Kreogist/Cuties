Kreogist/Cuties

commit c169b2eccd181b736366fca66b35dfb7088ce75c

Major: 0.3.0.13 Updates: 1. Complete the new panel framework. 2. Refactor Line Number and Smart Panel(Now become Unibody Panel).

src/app/kctexteditor.cpp
@@ -32,12 +32,6 @@
 #include "kccolorconfigure.h"
 #include "kctexteditor.h"
 
-KCPanelManager::KCPanelManager(QWidget *parent) :
-    QWidget(parent)
-{
-    ;
-}
-
 KCTextEditor::KCTextEditor(QWidget *parent) :
     QPlainTextEdit(parent)
 {
@@ -81,14 +75,17 @@ KCTextEditor::KCTextEditor(QWidget *parent) :
     connect(verticalScrollBar(),SIGNAL(valueChanged(int)),
             this,SLOT(updateHighlights()));
 
-    //panelManager=new KCPanelManager(this);
+    panelManager=new KCTextPanelManager(this);
 
-    lineNumberArea = new KCTextPanel(this);
+    lineNumberPanel=new KCLineNumberPanel(this);
+    panelManager->addPanel(lineNumberPanel);
+    unibodyPanel=new KCUnibodyPanel(this);
+    panelManager->addPanel(unibodyPanel);
 
     connect(this, SIGNAL(blockCountChanged(int)), this, SLOT(updateLineNumberAreaWidth(int)));
     connect(this, SIGNAL(updateRequest(QRect,int)), this, SLOT(updateLineNumberArea(QRect,int)));
-    connect(lineNumberArea, SIGNAL(requireRepaintLineNumber(QPaintEvent*)),
-            this, SLOT(lineNumberAreaPaintEvent(QPaintEvent*)));
+    connect(panelManager, SIGNAL(requirePaintPanel(KCTextPanel*,QPaintEvent*)),
+            this, SLOT(panelPaintEvent(KCTextPanel*,QPaintEvent*)));
 
     updateLineNumberAreaWidth(0);
 }
@@ -805,6 +802,11 @@ void KCTextEditor::updateHighlights()
     setExtraSelections(extraSelections);
 }
 
+void KCTextEditor::updatePanelManager()
+{
+    panelManager->update();
+}
+
 void KCTextEditor::highlightCurrentLine(QList<QTextEdit::ExtraSelection> &selections)
 { 
     if(!isReadOnly())
@@ -1283,7 +1285,7 @@ void KCTextEditor::zoomOut(int range)
     zoomIn(-range);
 }
 
-int KCTextEditor::lineNumberAreaWidth()
+int KCTextEditor::lineNumberPanelWidth()
 {
     int digits = 1;
     int max = qMax(1, blockCount());
@@ -1299,18 +1301,18 @@ int KCTextEditor::lineNumberAreaWidth()
 
 void KCTextEditor::updateLineNumberAreaWidth(int /* newBlockCount */)
 {
-    setViewportMargins(lineNumberAreaWidth(), 0, 0, 0);
+    setViewportMargins(panelManager->resizePanel(lineNumberPanelWidth()), 0, 0, 0);
 }
 
 void KCTextEditor::updateLineNumberArea(const QRect &rect, int dy)
 {
-    if (dy)
+    if(dy)
     {
-        lineNumberArea->scroll(0, dy);
+        //panelManager->scroll(0, dy);
     }
     else
     {
-        lineNumberArea->update(0, rect.y(), lineNumberArea->width(), rect.height());
+        panelManager->update(0, rect.y(), panelManager->width(), rect.height());
     }
 
     if (rect.contains(viewport()->rect()))
@@ -1324,29 +1326,30 @@ void KCTextEditor::resizeEvent(QResizeEvent *e)
     QPlainTextEdit::resizeEvent(e);
 
     QRect cr = contentsRect();
-    lineNumberArea->setGeometry(QRect(cr.left(), cr.top(), lineNumberAreaWidth(), cr.height()));
+    panelManager->setGeometry(QRect(cr.left(),
+                                    cr.top(),
+                                    panelManager->resizePanel(lineNumberPanelWidth()),
+                                    cr.height()));
 }
 
-void KCTextEditor::lineNumberAreaPaintEvent(QPaintEvent *event)
+void KCTextEditor::panelPaintEvent(KCTextPanel *panel,
+                                   QPaintEvent *event)
 {
-    QPainter painter(lineNumberArea);
-    painter.fillRect(event->rect(), Qt::lightGray);
-    QTextBlock block = firstVisibleBlock();
-    int blockNumber = block.blockNumber();
-    int top = (int) blockBoundingGeometry(block).translated(contentOffset()).top();
-    int bottom = top + (int) blockBoundingRect(block).height();
+    QTextBlock block=firstVisibleBlock();
+    int blockNumber=block.blockNumber();
+    int top=(int)blockBoundingGeometry(block).translated(contentOffset()).top();
+    int bottom=top+(int)blockBoundingRect(block).height();
 
-    while (block.isValid() && top <= event->rect().bottom()) {
-        if (block.isVisible() && bottom >= event->rect().top()) {
-            QString number = QString::number(blockNumber + 1);
-            painter.setPen(Qt::black);
-            painter.drawText(0, top, lineNumberArea->width(), fontMetrics().height(),
-                             Qt::AlignRight, number);
+    while (block.isValid() && top <= event->rect().bottom())
+    {
+        if (block.isVisible() && bottom >= event->rect().top())
+        {
+            panel->drawContent(0, top, panel->width(), fontMetrics().height(),
+                               block, textCursor());
         }
-
-        block = block.next();
-        top = bottom;
-        bottom = top + (int) blockBoundingRect(block).height();
+        block=block.next();
+        top=bottom;
+        bottom=top+(int)blockBoundingRect(block).height();
         ++blockNumber;
     }
 }

src/app/kctexteditor.h
@@ -34,7 +34,9 @@
 
 #include "kctextsearcher.h"
 #include "kcfloattoolbar.h"
-#include "kctextpanel.h"
+#include "kctextpanelmanager.h"
+#include "kclinenumberpanel.h"
+#include "kcunibodypanel.h"
 
 class KCClipboard;
 
@@ -57,7 +59,7 @@ public:
     void zoomIn(int range = 1);
     void zoomOut(int range = 1);
 
-    int lineNumberAreaWidth();
+    int lineNumberPanelWidth();
 
 signals:
     void requireHideOthers();
@@ -70,6 +72,7 @@ signals:
 public slots:
     void updateHighlights();
     //void pasteFromeHistory();
+    void updatePanelManager();
     bool showPreviousSearchResult();
     bool showNextSearchResult();
     void searchString(QString searchTextSets,
@@ -87,7 +90,8 @@ public slots:
 
 private slots:
     void updateSearchResults();
-    void lineNumberAreaPaintEvent(QPaintEvent *event);
+    void panelPaintEvent(KCTextPanel *panel,
+                                  QPaintEvent *event);
 
     void updateLineNumberAreaWidth(int newBlockCount);
     void updateLineNumberArea(const QRect &, int);
@@ -138,8 +142,6 @@ private:
     QColor noMatchedParenthesesColor;
     QColor matchedParenthesesColor;
 
-    KCPanelManager *panelManager;
-
     bool usingBlankInsteadTab=true;
     int spacePerTab=4;
 
@@ -155,7 +157,9 @@ private:
     QTextCursor searchBackupCursor;
     KCFloatToolBar *textFloatToolBar;
 
-    KCTextPanel *lineNumberArea;
+    KCTextPanelManager *panelManager;
+    KCLineNumberPanel *lineNumberPanel;
+    KCUnibodyPanel *unibodyPanel;
 };
 
 #endif // CODEEDITOR_H

src/app/kctextpanelmanager.cpp
@@ -1,0 +1,41 @@
+#include "kctextpanelmanager.h"
+
+KCTextPanelManager::KCTextPanelManager(QWidget *parent) :
+    QWidget(parent)
+{
+    setContentsMargins(0,0,0,0);
+    layout=new QHBoxLayout(this);
+    layout->setContentsMargins(0,0,0,0);
+    layout->setSpacing(0);
+    setLayout(layout);
+}
+
+void KCTextPanelManager::addPanel(KCTextPanel *panel)
+{
+    connect(panel, SIGNAL(requireRepaintLineNumber(KCTextPanel*,QPaintEvent*)),
+            this, SIGNAL(requirePaintPanel(KCTextPanel*,QPaintEvent*)));
+    layout->addWidget(panel);
+}
+
+int KCTextPanelManager::resizePanel(int lineNumberPanelWidth)
+{
+    int panelCount=layout->count(), managerWidth=0;
+    while(panelCount--)
+    {
+        KCTextPanel *panel=static_cast<KCTextPanel *>(layout->itemAt(panelCount)->widget());
+        panel->setPanelWidth(lineNumberPanelWidth);
+        managerWidth+=panel->width();
+    }
+    setFixedWidth(managerWidth);
+    return managerWidth;
+}
+
+void KCTextPanelManager::paintEvent(QPaintEvent *event)
+{
+    QWidget::paintEvent(event);
+    int panelCount=layout->count();
+    while(panelCount--)
+    {
+        layout->itemAt(panelCount)->widget()->update();
+    }
+}

src/app/kctextpanelmanager.h
@@ -1,0 +1,32 @@
+#ifndef KCTEXTPANELMANAGER_H
+#define KCTEXTPANELMANAGER_H
+
+#include <QHBoxLayout>
+#include <QWidget>
+#include <QPaintEvent>
+
+#include "kctextpanel.h"
+
+class KCTextPanelManager : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit KCTextPanelManager(QWidget *parent = 0);
+    void addPanel(KCTextPanel *panel);
+    int resizePanel(int lineNumberPanelWidth);
+
+signals:
+    void requirePaintPanel(KCTextPanel *panel,
+                           QPaintEvent *event);
+
+public slots:
+
+protected:
+    void paintEvent(QPaintEvent *event);
+
+private:
+    QHBoxLayout *layout;
+
+};
+
+#endif // KCTEXTPANELMANAGER_H

src/app/app.pro
@@ -111,7 +111,10 @@ SOURCES += \
     kcvisiblerecorder.cpp \
     Controls/GraphicButtons/kcgraphicbuttoncancel.cpp \
     kcdebugbreakpointmanager.cpp \
-    kctextpanel.cpp
+    kctextpanel.cpp \
+    kctextpanelmanager.cpp \
+    kclinenumberpanel.cpp \
+    kcunibodypanel.cpp
 
 HEADERS += \
     Controls/GraphicButtons/kcgraphicbuttonbase.h \
@@ -198,7 +201,10 @@ HEADERS += \
     kcvisiblerecorder.h \
     Controls/GraphicButtons/kcgraphicbuttoncancel.h \
     kcdebugbreakpointmanager.h \
-    kctextpanel.h
+    kctextpanel.h \
+    kctextpanelmanager.h \
+    kclinenumberpanel.h \
+    kcunibodypanel.h
 
 
 # Enable C++11

src/app/kccodeeditor.cpp
@@ -64,16 +64,8 @@ KCCodeEditor::KCCodeEditor(QWidget *parent) :
     markPanel=new KCMarkPanel(this);
     mainLayout->addWidget(markPanel);
 
-    linePanel=new KCLinenumPanel(this);
-    mainLayout->addWidget(linePanel);
-
-    smartPanel=new KCSmartPanel(this);
-    mainLayout->addWidget(smartPanel);
-
     editor=new KCTextEditor(this);
     markPanel->setTextEditor(editor);
-    linePanel->setTextEditor(editor);
-    smartPanel->setTextEditor(editor);
 
     mainLayout->addWidget(editor);
     replaceLayout->addLayout(mainLayout);
@@ -142,7 +134,7 @@ void KCCodeEditor::applyEditorSettings(KCCodeEditor::KCCodeEditorSettings settin
     editor->setTabStopWidth(fontMetrics().width(' ')*settings.tabSpace);
     editor->setUsingBlankInsteadTab(settings.usingBlankInsteadTab);
     editor->setSpacePerTab(settings.spacePerTab);
-    linePanel->setVisible(settings.lineNumberPanelVisible);
+    //linePanel->setVisible(settings.lineNumberPanelVisible);
 }
 
 bool KCCodeEditor::getOverwriteMode()
@@ -270,7 +262,6 @@ void KCCodeEditor::addErrorsToStack(int lineNum)
 void KCCodeEditor::redrawSmartPanel()
 {
     editor->setLineErrorState(errorOccurLines);
-    smartPanel->update();
 }
 
 void KCCodeEditor::showSearchBar()

src/app/kccodeeditor.h
@@ -153,9 +153,8 @@ private:
     QVBoxLayout *replaceLayout;
     QHBoxLayout *mainLayout;
     KCTextEditor *editor;
-    KCLinenumPanel *linePanel;
     KCMarkPanel *markPanel;
-    KCSmartPanel *smartPanel;
+    //KCSmartPanel *smartPanel;
 
     QString filePath;
     QString execFileName;

src/app/kclinenumberpanel.cpp
@@ -1,0 +1,48 @@
+
+#include <QDebug>
+#include <QPainter>
+
+#include "kclinenumberpanel.h"
+
+KCLineNumberPanel::KCLineNumberPanel(QWidget *parent) :
+    KCTextPanel(parent)
+{
+    textColor.setRgb(120,120,120);
+}
+
+void KCLineNumberPanel::drawContent(int x,
+                                    int y,
+                                    int width,
+                                    int height,
+                                    QTextBlock block,
+                                    QTextCursor cursor)
+{
+    QPainter painter(this);
+    bool isCurrentLine=cursor.blockNumber()==block.blockNumber();
+    if(isCurrentLine)
+    {
+        QFont font=painter.font();
+        font.setBold(true);
+        painter.setFont(font);
+
+        textColor.setRgb(255,255,255);
+    }
+    QPen pen(painter.pen());
+    pen.setColor(textColor);
+    painter.setPen(pen);
+    painter.drawText(x, y, width, height,
+                     Qt::AlignRight, QString::number(block.blockNumber() + 1));
+    if(isCurrentLine)
+    {
+        QFont font=painter.font();
+        font.setBold(false);
+        painter.setFont(font);
+
+        textColor.setRgb(120,120,120);
+    }
+}
+
+void KCLineNumberPanel::setPanelWidth(int lineNumberPanelWidth)
+{
+    setFixedWidth(lineNumberPanelWidth);
+}

commit e0703c01eefa9e33cb2cac2a4978447a30dce5b2

Major: 0.3.0.27
Bug Fixed:
1. Fixed #7 strange bug. This bug is really really interesting.
This is a bug is started from the commit
c169b2eccd181b736366fca66b35dfb7088ce75c.

At first, I think KCTextPanelManager should repaint all the KCTextPanel
at the time it repaint itself. But this cost a high CPU cost. I don't
realize what's happened.
Then I solved that bug by repaint all the panel manual. But I don't
notice that the difference between these two ways. The only different
is: All the panels is NOT repaint at time the panel manager repaint.
And the #7 mark panel bug is also cost by not update the data promptly.
The mark panel will only update at the time the block count changed from
9 to 10, or from 10 to 9. And the first block can use at every time. So
here's the point: There's a important thing we don't do is update the
panel.
There's a brand new way to update all the new panels and it's really
swift. And we don't need to use a loop this time: use a signal to notice
the panel to update. This signal will be connect at time when the panel
add to the manager. And the signal will be emit at the time
updateAllPanels() called in the panel manager. This slot will be called
at time KCTextEditor update.
All the things has been ended at here.

src/app/kctexteditor.cpp
@@ -89,10 +89,10 @@ KCTextEditor::KCTextEditor(QWidget *parent) :
             this,SLOT(updateHighlights()));
 
     /****Panels****/
-    connect(this, &KCTextEditor::blockCountChanged,
-            this, &KCTextEditor::updateLineNumberAreaWidth);
-    connect(this, &KCTextEditor::updateRequest,
-            this, &KCTextEditor::updateLineNumberArea);
+    connect(this, SIGNAL(blockCountChanged(int)),
+            this, SLOT(updatePanelAreaWidth(int)));
+    connect(panelManager, &KCTextPanelManager::requireUpdateAllPanel,
+            this, &KCTextEditor::updateAllPanels);
     connect(panelManager, &KCTextPanelManager::requirePaintPanel,
             this, &KCTextEditor::panelPaintEvent);
     connect(unibodyPanel, &KCUnibodyPanel::requireFoldStartAt,
@@ -102,9 +102,7 @@ KCTextEditor::KCTextEditor(QWidget *parent) :
     connect(lineNumberPanel, &KCLineNumberPanel::requireSelectBlock,
             this, &KCTextEditor::selectBlock);
 
-    //Called initialize widget procedure.
-    updateLineNumberAreaWidth(0);
-    panelManager->update();
+    //Update the high lights
     updateHighlights();
 }
 
@@ -795,9 +793,12 @@ void KCTextEditor::updateHighlights()
     setExtraSelections(extraSelections);
 }
 
-void KCTextEditor::updatePanelManager()
+void KCTextEditor::updateAllPanels()
 {
-    panelManager->update();
+    /*debugMarkPanel->update();
+    lineNumberPanel->update();
+    unibodyPanel->update();*/
+    panelManager->updateAllPanels();
 }
 
 void KCTextEditor::highlightCurrentLine(QList<QTextEdit::ExtraSelection> &selections)
@@ -1278,41 +1279,6 @@ void KCTextEditor::setDebugCursor(int lineNumber)
     debugMarkPanel->setDebugCursor(lineNumber);
 }
 
-int KCTextEditor::lineNumberPanelWidth()
-{
-    int digits = 1;
-    int max = qMax(1, blockCount());
-    while (max >= 10) {
-        max /= 10;
-        ++digits;
-    }
-
-    int space = 3 + fontMetrics().width(QLatin1Char('9')) * digits;
-
-    return space;
-}
-
-void KCTextEditor::setLinePanelVisible(bool value)
-{
-    lineNumberPanel->setVisible(value);
-    updateLineNumberAreaWidth(0);
-}
-
-void KCTextEditor::updateLineNumberAreaWidth(int /* newBlockCount */)
-{
-    setViewportMargins(panelManager->resizeManagerWidth(lineNumberPanelWidth()), 0, 0, 0);
-}
-
-void KCTextEditor::updateLineNumberArea(const QRect &rect, int dy)
-{
-    Q_UNUSED(dy);
-    panelManager->update();
-    if (rect.contains(viewport()->rect()))
-    {
-        updateLineNumberAreaWidth(0);
-    }
-}
-
 void KCTextEditor::foldCode(int startFoldBlockIndex)
 {
     QTextBlock foldBlock=document()->findBlockByNumber(startFoldBlockIndex);
@@ -1363,7 +1329,32 @@ void KCTextEditor::selectBlock(int blockNumber)
 void KCTextEditor::paintEvent(QPaintEvent *e)
 {
     QPlainTextEdit::paintEvent(e);
-    debugMarkPanel->update();
+    updateAllPanels();
+}
+
+int KCTextEditor::lineNumberPanelWidth()
+{
+    int digits = 1;
+    int max = qMax(1, blockCount());
+    while (max >= 10) {
+        max /= 10;
+        ++digits;
+    }
+
+    int space = 3 + fontMetrics().width(QLatin1Char('9')) * digits;
+
+    return space;
+}
+
+void KCTextEditor::setLinePanelVisible(bool value)
+{
+    lineNumberPanel->setVisible(value);
+    updatePanelAreaWidth(0);
+}
+
+void KCTextEditor::updatePanelAreaWidth(int /* newBlockCount */)
+{
+    setViewportMargins(panelManager->resizeManagerWidth(lineNumberPanelWidth()), 0, 0, 0);
 }
 
 void KCTextEditor::resizeEvent(QResizeEvent *e)

src/app/kctexteditor.h
@@ -80,7 +80,7 @@ signals:
 public slots:
     void updateHighlights();
     //void pasteFromeHistory();
-    void updatePanelManager();
+    void updateAllPanels();
     bool showPreviousSearchResult();
     bool showNextSearchResult();
     void searchString(QString searchTextSets,
@@ -102,8 +102,7 @@ private slots:
     void panelPaintEvent(KCTextPanel *panel,
                                   QPaintEvent *event);
 
-    void updateLineNumberAreaWidth(int newBlockCount);
-    void updateLineNumberArea(const QRect &, int);
+    void updatePanelAreaWidth(int newBlockCount);
     void foldCode(int startFoldBlockIndex);
     void unfoldCode(int startUnfoldBlockIndex);
     void selectBlock(int blockNumber);

src/app/kctextpanelmanager.cpp
@@ -51,7 +51,9 @@ void KCTextPanelManager::addPanel(KCTextPanel *panel)
     connect(panel, &KCTextPanel::requireRepaintPanel,
             this,  &KCTextPanelManager::requirePaintPanel);
     connect(panel, &KCTextPanel::requireUpdateAllPanel,
-            this,  &KCTextPanelManager::updateAllPanel);
+            this,  &KCTextPanelManager::requireUpdateAllPanel);
+    connect(this,  SIGNAL(requireUpdatePanel()),
+            panel, SLOT(update()));
     layout->addWidget(panel);
 }
 
@@ -80,19 +82,7 @@ int KCTextPanelManager::resizeManagerWidth(int lineNumberPanelWidth)
     return managerWidth;
 }
 
-/*!
- * \brief This function will repaint all the panels which beyond the
- *        KCTextPanelManager's control. In all the KCTextPanel, emit
- *        requireUpdateAllPanel() function can called this function.
- *        This function should not be called frequently, and you CAN'T called it
- *        inside paintEvent() function. Though Cuties won't crashed, it will
- *        caused a high CPU usage and stuck the UI.
- */
-void KCTextPanelManager::updateAllPanel()
+void KCTextPanelManager::updateAllPanels()
 {
-    int panelCount=layout->count();
-    while(panelCount--)
-    {
-        layout->itemAt(panelCount)->widget()->update();
-    }
+    emit requireUpdatePanel();
 }

src/app/kctextpanelmanager.h
@@ -47,9 +47,15 @@ public:
 signals:
     void requirePaintPanel(KCTextPanel *panel,
                            QPaintEvent *event);
+    /*!
+     * \brief This signal is used to ask text editor tell every panels to update
+     * themselves.
+     */
+    void requireUpdateAllPanel();
+    void requireUpdatePanel();
 
 public slots:
-    void updateAllPanel();
+    void updateAllPanels();
 
 protected:
 

src/app/main.cpp
@@ -50,7 +50,7 @@ static inline void loadApplicationInfo()
 {
     //Set application details.
     QApplication::setApplicationName(QString("Cuties"));
-    QApplication::setApplicationVersion(QString("0.3.0.26"));
+    QApplication::setApplicationVersion(QString("0.3.0.27"));
     QApplication::setOrganizationName("Kreogist Dev Team");
     QApplication::setOrganizationDomain("https://kreogist.github.io/Cuties");
 