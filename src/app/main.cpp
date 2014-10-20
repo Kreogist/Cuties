a66d65a78c2c813b829e85e464b68ce7458104df
Wang Luming <wlm199558@126.com>
2014-10-20 10:24:58 +0800

    test embed Python script into CutiesKether

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,7 +1,34 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <Python.h>
 
-int main()
+int main(int argc, char **argv)
 {
+    wchar_t *programName=new wchar_t[strlen(argv[0])+1];
+    mbstowcs(programName,argv[0],strlen(argv[0]));
+    Py_SetProgramName(programName);
+
+    Py_Initialize();
+
+    if(!Py_IsInitialized())
+    {
+        puts("Can not initialize python");
+        return -1;
+    }
+
+    FILE *pyMain=fopen("main.py","rw");
+    if(pyMain==NULL)
+    {
+        puts("main.py not find!");
+        return -1;
+    }
+
+    PyRun_SimpleFile(pyMain,"main.py");
+
+    Py_Finalize();
+
+    delete[] programName;
 
     return 0;
 }
